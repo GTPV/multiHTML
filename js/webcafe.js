482b8ba27816b3b5f24807208fa5f84e13f4d8fd
GTPV <parangvo@gmail.com>
2019-08-22 17:14:23 +0900

    HTML&CSS 4일차
     - 툴팁, 새창열기
     - 정의 리스트
     - 검색 input
     - tab contents
     - figure, caption

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,8 +1,21 @@
 let items = $('.menu-item');
+let tab = $('.tab');
+let section = $('.board section');
+let tabSubject = $('.board ul a')
 items.attr('tabindex', '0');
-
+tabSubject.addClass('icon-dot-circled');
+tab.attr('tabindex', '0');
 items.on('mouseover focusin', function(){
     items.removeClass('menu-act');
     $(this).addClass('menu-act');
 
+});
+
+
+tab.on('click keyup', function(e){
+    if(e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13)){
+        section.removeClass('board-act');
+        $(this).parent().addClass('board-act');
+    }
+    
 });
